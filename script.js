2766e250001a4b214e3a959fa4726ccab62f8ea9
Davi Lima <davi.limaqjl@gmail.com>
2026-02-26 02:42:45 -0300

    adicionando modo claro e modo escuro

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 alert("JavaScript está funcionando 😎")
+const button = document.getElementById("toggle-mode");
+const body = document.body;
+
+button.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  if(body.classList.contains("dark-mode")){
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+// carregar preferência ao abrir o site
+if(localStorage.getItem("theme") === "dark"){
+  body.classList.add("dark-mode");
+}
